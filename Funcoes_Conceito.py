29d44299cb25506c3cf5e384dcad5824a09c508f
miniguiti <gminiguiti@outlook.com>
2021-03-06 21:20:33 -0300

    Conceitos

---
+++ Funcoes_Conceito.py
@@ -0,0 +1,16 @@
+#Função de soma
+def soma(x, y):
+	return x+y
+
+s = soma(4, 3)
+print(s)
+
+#Função de multiplicação
+def multiplicação(x, y):
+	return x*y
+
+m = multiplicação(2, 3)
+print(m)
+
+#Usando funções recursivamente
+print(soma(s,m))
