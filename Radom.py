ead849dff1bedcee7df6a6d19a476e45fd7febff
miniguiti <gminiguiti@outlook.com>
2021-03-06 21:22:13 -0300

    Random

---
+++ Radom.py
@@ -0,0 +1,10 @@
+import random
+
+#Escholher número que um a 10
+numero = random.randint(0, 10)
+print(numero)
+
+#Escolher número que uma lista
+lista = [7,6,59,48]
+numero = random.choice(lista)
+print(numero)
